dbc2418b0dc6580575de052e0959e24e3a7713b2
amansharma112 <iamansharma112@gmail.com>
2024-11-18 12:25:14 +0530

    Add to resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -22,20 +22,20 @@
 //   export default DownloadButton;
   
 
-import React from 'react'
+// import React from 'react'
 
-export default function Resume() {
-  return (
-    <>
-    <div className=' md:gap-4  place-items-center pl-2 md:pr-2 pr-12'>
-            <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/" >Home</Link></div>
-            <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/about" >About-Me</Link></div>
-            <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/project" >Project</Link></div>
-            <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/contact" >Contact</Link></div>
-            <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/blog" >Blog</Link></div>
-            <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/skill" >Skill</Link></div>
+// export default function Resume() {
+//   return (
+//     <>
+//     <div className=' md:gap-4  place-items-center pl-2 md:pr-2 pr-12'>
+//             <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/" >Home</Link></div>
+//             <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/about" >About-Me</Link></div>
+//             <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/project" >Project</Link></div>
+//             <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/contact" >Contact</Link></div>
+//             <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/blog" >Blog</Link></div>
+//             <div className='hover:bg-sky-100  pl-2 pr-2 rounded-full'><Link rel="stylesheet" href="/skill" >Skill</Link></div>
 
-          </div>
-    </>
-  )
-}
+//           </div>
+//     </>
+//   )
+// }
